29d1c1d5970d203928a2bed49631a26bfc08bffa
zeineb sghaier <zeineb.sghaier@esprit.tn>
2026-02-15 19:42:09 +0100

    integration +google auth+likescomments

--- config/bundles.php
+++ config/bundles.php
@@ -5,11 +5,8 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-<<<<<<< HEAD
-=======
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
->>>>>>> validation-final-2
 ];
